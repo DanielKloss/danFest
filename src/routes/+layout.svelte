82dfb9221cee0335eccda9cac2c57e96f76adf1d
Daniel Kloss <arsenal_fan_dan@hotmail.com>
2022-08-31 16:09:41 +0100

    added loading spinner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,20 @@
 <script>
+  import { Circle } from 'svelte-loading-spinners'
+  import { navigating } from '$app/stores'
   import "../app.css";
-
   import NavBar from "$lib/navBar.svelte"; 
 </script>
 
 <NavBar/>
 
 <main>
-  <slot />
+  {#if $navigating}
+    <div style="display: flex; justify-content:center">
+      <Circle color="#DE0000"/>
+    </div>
+  {:else}
+    <slot />
+  {/if}
 </main>
 
 <style>
